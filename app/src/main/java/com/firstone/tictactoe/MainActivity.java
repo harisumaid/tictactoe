43922a967d823925aa86a02351d6e095b2cfe592
harisumaid <harisumaid27@.com>
2018-09-11 22:20:58 +0530

    final structure

--- app/src/main/java/com/firstone/tictactoe/MainActivity.java
+++ app/src/main/java/com/firstone/tictactoe/MainActivity.java
@@ -11,8 +11,13 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     int chance=0;
+    int score1=0;
+    int score2=0;
+    int check=0;
     int state[]={2,2,2,2,2,2,2,2,2};
     int winstates [][]= {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
+
+
 
     public void reset(View view)
     {
@@ -20,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         LinearLayout layout = (LinearLayout) findViewById(R.id.playagain);
         layout.setVisibility(View.INVISIBLE);
         chance =0;
-
+        check=1;
         for(int i=0; i<state.length;i++)
             state[i]=2;
         android.support.v7.widget.GridLayout gridLayout = (android.support.v7.widget.GridLayout) findViewById(R.id.grid);
@@ -33,52 +38,67 @@ public class MainActivity extends AppCompatActivity {
 
     public void ingame(View view) {
         // true == tick   false == cross
-        ImageView game = (ImageView) view;
-        if (state[Integer.parseInt(String.valueOf(game.getTag()))] == 2) {
-        game.setScaleX(0f);
-        game.setScaleY(0f);
+        TextView player1=(TextView) findViewById(R.id.player1);
+        TextView player2=(TextView) findViewById(R.id.player2);
 
-            state[Integer.parseInt(String.valueOf(game.getTag()))]=chance;
-            if (chance == 0) {
-                game.setImageResource(R.drawable.tick);
-                chance = 1;
+        if (check!=9) {
+            ImageView game = (ImageView) view;
+            if (state[Integer.parseInt(String.valueOf(game.getTag()))] == 2) {
+                game.setScaleX(0f);
+                game.setScaleY(0f);
+
+                state[Integer.parseInt(String.valueOf(game.getTag()))] = chance;
+                if (chance == 0) {
+                    game.setImageResource(R.drawable.tick);
+                    chance = 1;
+                } else {
+                    game.setImageResource(R.drawable.cross);
+                    chance = 0;
+                }
+                game.animate().rotation(360)
+                        .scaleX(1f)
+                        .scaleY(1f)
+                        .setDuration(500);
             } else {
-                game.setImageResource(R.drawable.cross);
-                chance = 0;
+                Toast.makeText(this, "box already filled !!!! ", Toast.LENGTH_SHORT).show();
             }
-            game.animate().rotation(360)
-                    .scaleX(1f)
-                    .scaleY(1f)
-                    .setDuration(500);
-        }
-        else{
-            Toast.makeText(this, "box already filled !!!! ", Toast.LENGTH_SHORT).show();
-        }
 
-        for(int gamestate[]: winstates){
-            if (state[gamestate[0]]==state[gamestate[1]]&&state[gamestate[1]]==state[gamestate[2]]
-                    &&state[gamestate[0]]!=2){
-                TextView winnermessage= (TextView) findViewById(R.id.winnermessage);
-                if (state[gamestate[0]]==0) {
-                    Toast.makeText(this, "Player 1 striked player 2!!!!!", Toast.LENGTH_LONG).show();
-                    winnermessage.setText("Player 1 won !!!!");
-                }
-                    else {
-                    Toast.makeText(this, "Player 2 circled Player 1!!!!!", Toast.LENGTH_LONG).show();
-                    winnermessage.setText("Player 2 won !!!!");
-                }
+            for (int gamestate[] : winstates) {
+                if (state[gamestate[0]] == state[gamestate[1]] && state[gamestate[1]] == state[gamestate[2]]
+                        && state[gamestate[0]] != 2) {
+                    TextView winnermessage = (TextView) findViewById(R.id.winnermessage);
+                    if (state[gamestate[0]] == 0) {
+                        Toast.makeText(this, "Player 1 striked player 2!!!!!", Toast.LENGTH_LONG).show();
+                        winnermessage.setText("Player 1 won !!!!");
+                        score1++;
+                    } else {
+                        Toast.makeText(this, "Player 2 circled Player 1!!!!!", Toast.LENGTH_LONG).show();
+                        winnermessage.setText("Player 2 won !!!!");
+                        score2++;
+                    }
                     LinearLayout layout = (LinearLayout) findViewById(R.id.playagain);
-                layout.setVisibility(View.VISIBLE);
+                    layout.setVisibility(View.VISIBLE);
+                }
+                player1.setText("PLAYER 1 :"+ score1);
+                player2.setText("PLAYER 2 :"+ score2);
             }
+
+            check++;
         }
-
+        else if (check==9){
+            LinearLayout layout = (LinearLayout) findViewById(R.id.playagain);
+            TextView winnermessage =(TextView) findViewById(R.id.winnermessage);
+            winnermessage.setText("Match Draw !!!!");
+            layout.setVisibility(View.VISIBLE);
+        }
     }
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "1st to play gets tick sign", Toast.LENGTH_LONG).show();
+
     }
 }
